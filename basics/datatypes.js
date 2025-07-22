5ae3f9668cfa44696223d5ca1f39bc8c1cd44377
Aditya Kumar <adikr4477@gmail.com>
2025-07-22 11:21:22 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version
+// alert (3 + 3) //we are using nodejs not browser
+console.log(3+3)
+console.log("Aditya Kumar") // code readebilty must be high
+
+
+let name ="Aditya"
+let age = 20
+let IsLoggedin = true
+// number =>2 to 53
+//bigint
+//boolean=> true/false
+//null => standalone value
+//symbol=>  unique
+
+//object
+
+
+
+console.log(typeof "Aditya")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 234612
+let accountEmail = "aditya@123"
+var accountPassword = "9955"
+accountCity = "Delhi"
+let accountState;
+accountPassword = "3725"
+accountEmail= "hello@123"
+accountCity = "Mumbai"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/* 
+prefer not use var
+because it create isssue in block code and functional scope
+*/
