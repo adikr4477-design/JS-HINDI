506173555ae780aeff3fad5e7dca18730e547d65
Aditya Kumar <adikr4477@gmail.com>
2025-07-24 10:29:57 +0000

    conversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+//console.log(typeof score);
+//console.log(typeof (score));
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true => 01
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true
+// 0 => false
+//"" => falase
+//"Aditya" => ture
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
