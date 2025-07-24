4476a9448d264a614eeae3d3fb897af9178faab8
Aditya Kumar <adikr4477@gmail.com>
2025-07-24 11:32:11 +0000

    more on conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 let isLoggedIn =1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 // 1 => true
 // 0 => false
 //"" => falase
@@ -20,5 +20,22 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**************OPERATION******************
+let value = 3
+let negValue = -value
+//console.log(negValue);
+let str1 = "HELLO "
+let str2 = "Aditya"
+let str3 = str1+str2
+console.log(str3);
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
