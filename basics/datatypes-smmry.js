201f89e65c4996a36fdca2d689d1ba756d980c42
Aditya Kumar <adikr4477@gmail.com>
2025-07-24 12:40:30 +0000

    summary

---
+++ basics/datatypes-smmry.js
@@ -0,0 +1,23 @@
+//primitive
+//7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const id = Symbol ('123')
+const anotherId = Symbole('123')
+
+console.log(id === anotherId);
+const bigNumber = 3473856234865237846286208602206n
+
+
+
+//Refermce type  (non primitive)
+//Array, Object, Function
+// Javascript is dynamically datatype language
+const heros = ["Shaktiman", "Nargraj", "Doga"] // Array
+ let myObj{
+    name: Aditya,
+    age: 20
+}
+
+const myFunction = function(){
+    console.log("HELLO WORLD!");
+}
