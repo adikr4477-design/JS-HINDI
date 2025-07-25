aee3d16ba1089e9eef8fdb28e78b392dec3cb450
Aditya Kumar <adikr4477@gmail.com>
2025-07-25 04:44:05 +0000

    aaded

--- basics/datatypes-smmry.js
+++ basics/datatypes-smmry.js
@@ -1,23 +1,30 @@
 //primitive
 //7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
-const id = Symbol ('123')
-const anotherId = Symbole('123')
+//const id = Symbol ('123')
+//const anotherId = Symbole('123')
 
-console.log(id === anotherId);
-const bigNumber = 3473856234865237846286208602206n
+//console.log(id === anotherId);
+//const bigNumber = 3473856234865237846286208602206n
 
 
 
 //Refermce type  (non primitive)
 //Array, Object, Function
 // Javascript is dynamically datatype language
-const heros = ["Shaktiman", "Nargraj", "Doga"] // Array
- let myObj{
-    name: Aditya,
-    age: 20
-}
+ 
 
-const myFunction = function(){
-    console.log("HELLO WORLD!");
+
+//************************************************************
+//Stack(primitive), Heap(Nonprimitive)
+let mySchoolName = "Aditya"
+let anothername = mySchoolName
+console.log(anothername);
+let userOne = {
+    email : "user@123",
+    onlinepayment : "ybl@123" 
 }
+let userTwo = userOne
+userTwo.email = "Aditya@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
