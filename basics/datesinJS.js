8f37af8ea6e125c1a4c2107ce9f447d769438e7b
Aditya Kumar <adikr4477@gmail.com>
2025-07-26 08:41:05 +0000

    dinank

---
+++ basics/datesinJS.js
@@ -0,0 +1,22 @@
+//DATES (date is  object datatypes)
+//In  JAVASCRIPT month start from zero
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString);
+// console.log(myDate.toDateString);
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+
+
+let myCreatedDate = new Date("2023-01-22")
+console.log(myCreatedDate);
+
+// let myTimeStamp = Date.now()
+// console.log(myCreatedDate.getTime);
+// console.log(Math.floor(Date.now()/100));
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
