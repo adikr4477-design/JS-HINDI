770fb48728e14070fd0f9689ff1d68ca18d46b6f
Aditya Kumar <adikr4477@gmail.com>
2025-07-26 06:17:45 +0000

    strong concept

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const hunderds = 10000000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++MATHS++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil (5.3));
+// console.log(Math.floor(3.9));
+// console.log(Math.max(2,2,4,45,3,5));
+// console.log(Math.min(1,342,675632,0.1));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()* (max-min +1))+min);
+
